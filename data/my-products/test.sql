e2dc9acaa26986262ab598e54c348ba453ddd39a
wun <kevinlay841009@gmail.com>
2024-04-13 15:44:28 +0800

    0413  第五節課

--- data/my-products/test.sql
+++ data/my-products/test.sql
@@ -25,3 +25,25 @@ WHERE price >= 10000 AND price <= 15000;
 SELECT * 
 FROM my_product 
 WHERE price BETWEEN 10000 AND 15000;
+
+
+-- 排序: 價格依低到高(ASC) 或高到低(DESC) (查詢字串qs: sort=price&order=asc)
+SELECT *
+FROM my_product
+ORDER BY price ASC;
+-- ORDER BY price DESC;
+
+-- 分頁: 目前是第page頁，每頁perpage個( (查詢字串qs: page=1&perpage=5)
+-- 套用公式:
+-- limit = perpage
+-- page = 1 offset = 0 ; page=2 offset=perpage*1 ===> offset=(page-1)*perpage
+-- 依查詢條件查出目前頁數的資料
+SELECT *
+FROM my_product
+WHERE brand_id IN(1,2)
+LIMIT 2 OFFSET 0;
+
+-- 計算目前的條件下有多少結果(註:回應total筆數和pageCount總頁數)
+SELECT COUNT(*)
+FROM my_product
+WHERE brand_id IN (1,2)
--- routes/my-products.js
+++ routes/my-products.js
@@ -8,13 +8,105 @@ import { getIdParam } from '#db-helpers/db-tool.js'
 import sequelize from '#configs/db.js'
 const { My_Product } = sequelize.models
 
-// GET - 得到所有資料
+// 一般sql
+import db from '#configs/mysql.js'
+
+// 列表頁
+// my-products?brand_ids=1,2&name_like=pixel&price_gte=10000&price_lte=15000&sort=price&order=asc&page=1&perpage=5
 router.get('/', async function (req, res) {
-  const products = await My_Product.findAll({ logging: console.log })
+  // 先解構獲取每個query string的值
+  const {
+    page = 1, // number,  用於 OFFSET =  (Number(page) - 1) * Number(perpage),
+    perpage = 10, // number, 用於 LIMIT
+    name_like = '', // string, 對應 name 欄位, `name LIKE '%name_like%'`
+    brand_ids = '', // string, 對應 brand_id 欄位,  `brand_id IN (brand_ids)`
+    sort = 'price', // string, 排序欄位 用於 ORDER BY
+    order = 'asc', // string, 排序順序 用於 ORDER BY 'asc' | 'desc', 預設為'asc'
+    price_gte = 5000, // number, 對應 price 欄位, `price >= 1500`
+    price_lte = 100000, // number, 對應 price 欄位, `price <= 10000` } = req.query
+  } = req.query
+
+  //  測試用
+  // console.log(
+  //   page,
+  //   perpage,
+  //   name_like,
+  //   brand_ids,
+  //   sort,
+  //   order,
+  //   price_gte,
+  //   price_lte
+  // )
+
   // 處理如果沒找到資料
 
+  // 建立資料庫搜尋條件(where從句用)，每個條件用陣列存放，串接時用join(' AND ')
+  const conditions = []
+
+  // 名稱  LIKE is not case sensitive(英文大小寫無關)
+  conditions[0] = name_like ? `name LIKE '%${name_like}%'` : ''
+
+  // 品牌，brand_ids 使用 `brand_id IN (1,2,3)`
+  conditions[1] = brand_ids ? `brand_id IN (${brand_ids})` : ''
+
+  // 價格大於，price_gte 使用 `price >= 5000`
+  conditions[2] = price_gte ? `price >= ${price_gte}` : ''
+
+  // 價格小於，price_lte 使用 `price <= 10000`
+  conditions[3] = price_lte ? `price <= ${price_lte}` : ''
+
+  // 去除空字串
+  const conditionsValues = conditions.filter((v) => v)
+
+  // 各條件需要先包含在`()`中，因各自內查詢是OR, 與其它的是AND
+  const where =
+    conditionsValues.length > 0
+      ? `WHERE ` + conditionsValues.map((v) => `( ${v} )`).join(` AND `)
+      : ''
+
+  // 排序用
+  const orderby = `ORDER BY ${sort} ${order}`
+
+  // 分頁用
+  // page預設為1，perpage預設為3
+  const perpageNow = Number(perpage) || 3
+  const pageNow = Number(page) || 1
+  const limit = perpageNow
+  // page=1 offset=0; page=2 offset= perpage * 1; ...
+  const offset = (pageNow - 1) * perpageNow
+
+  // 最終組合的sql語法
+  const sqlProducts = `SELECT * FROM my_product ${where} ${orderby} LIMIT ${limit} OFFSET ${offset}`
+  // 最終組合的sql語法(計數用)
+  const sqlCount = `SELECT COUNT(*) AS count FROM my_product ${where}`
+
+  // 顯示sql語法
+  console.log(sqlProducts)
+  console.log(sqlCount)
+
+  //const products = await My_Product.findAll({ logging: console.log })
+
+  // 使用原本的mysql2+sql的查詢方式
+  const [rows, fields] = await db.query(sqlProducts)
+
+  console.log(rows)
+
+  const [rows2] = await db.query(sqlCount)
+  // 回傳總筆數
+  const total = rows2[0].count
+
+  // 計算頁數
+  const pageCount = Math.ceil(total / Number(perpage)) || 0
+
   // 標準回傳JSON
-  return res.json({ status: 'success', data: { products } })
+  return res.json({
+    status: 'success',
+    data: {
+      total,
+      pageCount,
+      products: rows,
+    },
+  })
 })
 
 // GET - 得到單筆資料(注意，有動態參數時要寫在GET區段最後面)
